470c9d6ed8e759e63d43238810fdfbeee0549832
RiccardoCivardi <riccardo.civardi@icloud.com>
2023-01-12 19:35:48 +0100

    [add] resource route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// per importare use App\Http\Controllers\ComicController; dare invio dopo aver scritto il nome del controller
+Route::resource('comics', ComicController::class);
